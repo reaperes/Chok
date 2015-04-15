79bfc867cfaa2be5b97008fbce44199bb2196ce7
마크(채종운) <cha452jg@coupang.com>
2015-04-15 21:08:38 +0900

    함수로 분리

--- crawler/main/main.py
+++ crawler/main/main.py
@@ -8,6 +8,7 @@ auth = tweepy.OAuthHandler("lnxAnwy8TTyknuqgE2F6yMYG9", "hmQJZBcZSVcO9l93FAjjazZ
 auth.set_access_token("53275469-VIy3G3etiqG7NrqdGi7qjgHVcH6ve93eAewkWec0W", "QZZsJFlZEsFvdoXZJpD8UX7Nzn3Qnc9dj6scavMISrSjQ")
 
 api = tweepy.API(auth)
+
 ##konlpy의 twitter 형태소 분석기 사용
 twitter = Twitter()
 
@@ -17,24 +18,39 @@ cur = conn.cursor()
 
 mealList = ["짜장면", "탕수육", "짬뽕", "군만두", "팹시", "아이스크림", "부대찌개", "국밥", "콩나물", "두부", "타리미슈", "케이크", "치킨"];
 
-##인코딩 세팅 작업 진행(ex : http://libsora.so/posts/python-hangul/)
+# # 선릉역 10km 주변 트윗검색
+# for tweet in tweepy.Cursor(api.search,
+#                            q="",
+#                            rpp=100,
+#                            geocode="37.504525,127.048902,1km",
+#                            result_type="recent",
+#                            include_entities=True,
+#                            lang="ko").items():
+#     for text in twitter.morphs(tweet.text) :
+#         for meal in mealList :
+#             if(meal == text) :
+#                 print("mealName : " + meal)
+#                 print("text : " + tweet.text)
+#                 cur.execute("INSERT INTO test_data VALUES ('%s', '%s')" % (meal, tweet.text))
+#
+#
+# conn.commit()
+# conn.close()
+
+def getConnection(ip, userName, pwd, DBName):
+    conn = pymysql.connect(host=ip, unix_socket='/tmp/mysql.sock', user=userName, passwd=pwd, db=DBName, charset='utf8')
+    return conn;
+
+def getCur(conn):
+    return conn.cursor()
+
+def searchTweet():
+    return tweepy.Cursor(api.search, q="", rpp=100, geocode="37.504525,127.048902,1km", result_type="recent", include_entities=True, lang="ko").items()
+
+def divideTweet(tweet):
+    return twitter.morphs(tweet.text)
 
 
-# 선릉역 10km 주변 트윗검색
-for tweet in tweepy.Cursor(api.search,
-                           q="",
-                           rpp=100,
-                           geocode="37.504525,127.048902,1km",
-                           result_type="recent",
-                           include_entities=True,
-                           lang="ko").items():
-    for text in twitter.morphs(tweet.text) :
-        for meal in mealList :
-            if(meal == text) :
-                print("mealName : " + meal)
-                print("text : " + tweet.text)
-                cur.execute("INSERT INTO test_data VALUES ('%s', '%s')" % (meal, tweet.text))
-
-
-conn.commit()
-conn.close()
+list = searchTweet();
+for tweet in list:
+    print tweet.text
